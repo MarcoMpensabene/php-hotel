8b68f1585f2534282073b24628f90ffa1db33125
MarcoMpensabene <marcompensabene@gmail.com>
2024-06-27 14:33:42 +0200

    printed element without any style

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php   include_once __DIR__ . "/utilities/hotelsList.php";   
+
+foreach($hotels as $element){
+    echo $element["name"];
+    echo $element["description"];
+    echo $element["parking"];
+    echo $element["vote"];
+    echo $element["distance_to_center"];
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +22,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
+    
 <table class="table">
   <thead>
     <tr>
